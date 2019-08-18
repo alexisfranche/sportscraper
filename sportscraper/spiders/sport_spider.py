1beff98ae6b26d9ff42a3474e6d805830f3b4d71
Alexis Franche <alexis.franche@mail.mcgill.ca>
2019-08-18 15:32:06 -0400

    completed framework parse method and addes helper string parse method

--- sportscraper/spiders/sport_spider.py
+++ sportscraper/spiders/sport_spider.py
@@ -13,13 +13,23 @@ class SportSpider(scrapy.Spider):
         ctn = body.css("tr")
         for data in ctn.css("tr"):
             infoNode = data.css("td")
-            print(infoNode)
+            print(self.parseNode("-------------------" + infoNode[0].get().decode("latin-1", "replace") + "---------------"))
             yield {
-                    'Event date': str(infoNode[0].get()),
-                    'Time': str(infoNode[1].get()),
-                    'Local team': str(infoNode[2].get()),
-                    'Points Local': str(infoNode[3].get()),
-                    'Visitor Team': str(infoNode[4].get()),
-                    'Points Visitors': str(infoNode[5].get()),
-                    'Location': str(infoNode[6].get())
+                    'Event date': self.parseNode((infoNode[0].get())),
+                    'Time': self.parseNode((infoNode[1].get())),
+                    'Local team': self.parseNode((infoNode[2].get())),
+                    'Points Local': self.parseNode((infoNode[3].get())),
+                    'Visitor Team': self.parseNode((infoNode[4].get())),
+                    'Points Visitors': self.parseNode((infoNode[5].get())),
+                    'Location': self.parseNode((infoNode[6].get()))
                 }
+
+    def parseNode(self, strNode):
+        index = strNode.find("<")
+        index2 = strNode.find(">")
+
+        if index == -1:
+            return strNode
+        else:
+            strNodeFixed = strNode[0:index] + strNode[index2+1:]
+            return self.parseNode(strNodeFixed)
