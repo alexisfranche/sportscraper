1f76d7b0de0a54a3ab5b1ef147fdb335bd1025c1
Alexis Franche <alexis.franche@mail.mcgill.ca>
2019-08-19 00:01:01 -0400

    working on google api request, fix api denied request to work

--- sportscraper/spiders/sport_spider.py
+++ sportscraper/spiders/sport_spider.py
@@ -1,5 +1,8 @@
 import scrapy
+import requests
 
+baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address="
+api_key="AIzaSyAglwogS08ArRJ7rUQu7xu_yGJxr6ZanZI"
 
 class SportSpider(scrapy.Spider):
     name = "sport"
@@ -12,9 +15,21 @@ class SportSpider(scrapy.Spider):
         table = response.css("#content > div.entry-content > table:nth-child(1)")
         body = table.css("tbody")
         ctn = body.css("tr")
-        
-        for data in ctn.css("tr"):
+        dataList = ctn.css("tr")
+        dataList.pop(0) #remove table headers
+        for data in dataList:
             infoNode = data.css("td")
+
+            #linkLoc = self.parseMapsLink(infoNode[6].css("a::attr(href)").get())
+
+            Address = self.parseMapsLink(infoNode[6].css("a::attr(href)").get())
+            if Address is not None:
+                info = requests.get(baseUrl + Address + "&key=" +api_key)
+                resp_json_payload = info.json()
+                print(resp_json_payload)
+            latitude = None
+            longitude = None
+
             yield {
                     'Event date': self.parseNode((infoNode[0].get())),
                     'Time': self.parseNode((infoNode[1].get())),
@@ -22,7 +37,10 @@ class SportSpider(scrapy.Spider):
                     'Points Local': self.parseNode((infoNode[3].get())),
                     'Visitor Team': self.parseNode((infoNode[4].get())),
                     'Points Visitors': self.parseNode((infoNode[5].get())),
-                    'Location': self.parseNode((infoNode[6].get()))
+                    'Location Name': self.parseNode((infoNode[6].get())),
+                    'Location Address': Address,
+                    'Latitude': latitude,
+                    'Longitude': longitude,
                 }
 
     #Method added to parse text content retrieved (remove tags).
@@ -35,3 +53,20 @@ class SportSpider(scrapy.Spider):
         else:
             strNodeFixed = strNode[0:index] + strNode[index2+1:]
             return self.parseNode(strNodeFixed)
+
+    def parseMapsLink(self, link):
+        if link is not None:
+            index = link.find("place/")
+
+            if index == -1:
+                return None
+            else:
+                addressTemp = link[index+6:]
+                index2 = addressTemp.find("/")
+                addressTemp = addressTemp[:index2]
+                #address = addressTemp.replace("+", " ")
+
+                return addressTemp
+        else:
+            return None
+
